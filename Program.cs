ab3c86aabb84a4be24cab74f0164c0f431659f2a
DESKTOP-61GVAIP\Pandian <pandiarajan.it@gmail.com>
2018-07-28 17:07:13 +0530

    Commiting with unit tests

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Linq;
 
 namespace VsCodeNew
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program program = new Program();
+            Console.WriteLine("Please enter any number:");
+            int n = Convert.ToInt32(Console.ReadLine());        
+            Console.WriteLine($"Sum of bits in {n}:");
+            Console.WriteLine(program.GetPopulationCount(n));
+        }
+        public int GetPopulationCount(int number)
+        {
+            return Convert.ToString(number,2).ToCharArray().Sum(c => Convert.ToInt32(c.ToString()));
         }
     }
 }
